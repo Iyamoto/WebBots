22e903cfba78c371f3b089be307abbe8ceddefdf
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-06 22:16:58 +0400

    infinite marks added

--- avito/avito.php
+++ avito/avito.php
@@ -8,34 +8,48 @@ $debug_file = 'debug.html';
 $url = "http://www.avito.ru/sankt-peterburg/ohota_i_rybalka?metro_id=170&user=1&s=1";
 $in = http_get_debug($url,$debug_file);
 $tidy = tidy_html($in);
-$html = str_get_html($tidy);
 
-$mark1 = 'img';
-$mark2 = 'title';
-$mark3 = 'руб';
-$i=0;
-$sum=0;
-foreach($html->find('div') as $element) {
-    $div = $element->innertext;
-	if(stristr($div, $mark1) and stristr($div, $mark2) and stristr($div, $mark3)) {
-		$divs[$i]['html'] = $div;
-		$divs[$i]['size'] = strlen($div);
-		$sum+= $divs[$i]['size'];
-		$i++;
+$marks[] = 'img';
+$marks[] = 'руб';
+$good_divs = get_divs($tidy,$marks);
+
+var_dump($good_divs);
+
+function get_divs($str,$marks){
+	$html = str_get_html($str);
+	$i=0;
+	$sum=0;
+	foreach($html->find('div') as $element) {
+		$div = $element->innertext;
+		$m = sizeof($marks);
+		$c=0;
+		foreach($marks as $mark){
+			if(stristr($div, $mark)) $c++;
+		}
+		if ($c == $m) {
+			$divs[$i]['html'] = $div;
+			$divs[$i]['size'] = strlen($div);
+			$sum+= $divs[$i]['size'];
+			$i++;		
+		}
 	}
+	$html->clear();
+	unset($html);
+	
+	$average = round($sum/$i);
+	echo "[+]Sum $sum\n";
+	echo "[+]Average $average\n";
+	echo "[+]Found $i div blocks\n";
+
+	$g=0;
+	foreach($divs as $n=>$div){
+		if($div['size']<$average) {
+			$good_divs[] = $div['html'];
+			$g++;
+		}	
+	}
+	echo "[+]Found $g good div blocks\n";
+	return $good_divs;
 }
-$average = round($sum/$i);
-echo "[+]Sum $sum\n";
-echo "[+]Average $average\n";
-echo "[+]Found $i div blocks\n";
-$html->clear();
-$g=0;
-foreach($divs as $n=>$el){
-	if($el['size']<$average) {
-		$good_div[] = $el['html'];
-		$g++;
-	}	
-}
-echo "[+]Found $g good div blocks\n";
 
 ?>
