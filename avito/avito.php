8772a9d1731871245b0aa10c03c7d6f1a5d08a94
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-06 21:57:06 +0400

    div parser added, html dom class added

--- avito/avito.php
+++ avito/avito.php
@@ -1,36 +1,41 @@
 ﻿<?php
 require_once '..\libs\LIB_parse.php';
 require_once '..\libs\LIB_http.php';
+require_once '..\libs\simple_html_dom.php';
 echo "\n[+]Started\n";
 
 $debug_file = 'debug.html';
 $url = "http://www.avito.ru/sankt-peterburg/ohota_i_rybalka?metro_id=170&user=1&s=1";
 $in = http_get_debug($url,$debug_file);
+$tidy = tidy_html($in);
+$html = str_get_html($tidy);
 
-/*
-$mark1 = 'photo';
+$mark1 = 'img';
 $mark2 = 'title';
-$mark3 = 'description';
-foreach($divs->children() as $tmp){
-	var_dump($tmp);
-	$div = $tmp->plaintext;
-	//if(stristr($div, $mark1) and stristr($div, $mark2) and stristr($div, $mark3)) {
-	if(stristr($div, $mark1)){
-		var_dump($div);
+$mark3 = 'руб';
+$i=0;
+$sum=0;
+foreach($html->find('div') as $element) {
+    $div = $element->innertext;
+	if(stristr($div, $mark1) and stristr($div, $mark2) and stristr($div, $mark3)) {
+		$divs[$i]['html'] = $div;
+		$divs[$i]['size'] = strlen($div);
+		$sum+= $divs[$i]['size'];
+		$i++;
 	}
 }
-*/
-
-function get_div($str,$needle='<div'){
-	if(stristr($str, $div)){
-		//есть еще дивы, идем в рекурсию
-		get_div($str1,$needle);
-	} else {
-		//больше дивов нет, возвращаем
-		return $something;
-	}
-	//проверка на конец строки
-	//вычисление позиций
+$average = round($sum/$i);
+echo "[+]Sum $sum\n";
+echo "[+]Average $average\n";
+echo "[+]Found $i div blocks\n";
+$html->clear();
+$g=0;
+foreach($divs as $n=>$el){
+	if($el['size']<$average) {
+		$good_div[] = $el['html'];
+		$g++;
+	}	
 }
+echo "[+]Found $g good div blocks\n";
 
 ?>
