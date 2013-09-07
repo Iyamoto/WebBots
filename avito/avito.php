7863fff78d4f7e07315ba35bc6711c259a338f0b
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-07 07:41:28 +0400

    web_bots.php added

--- avito/avito.php
+++ avito/avito.php
@@ -1,7 +1,5 @@
 ﻿<?php
-require_once '..\libs\LIB_parse.php';
-require_once '..\libs\LIB_http.php';
-require_once '..\libs\simple_html_dom.php';
+require_once '..\libs\web_bots.php';
 echo "\n[+]Started\n";
 
 $debug_file = 'debug.html';
@@ -12,10 +10,6 @@ $tidy = tidy_html($in);
 $div_marks[] = 'img';
 $div_marks[] = 'руб';
 $good_divs = get_divs($tidy,$div_marks);
-
-$price_marks[] = 'руб.';
-$price_marks[] = 'рублей';
-
 
 for($i=0;$i<count($good_divs);$i++){
 	$data[$i]['imgs']= get_imgs($good_divs[$i]);
@@ -29,89 +23,8 @@ for($i=0;$i<count($good_divs);$i++){
 var_dump($data);
 
 //get type of item (taxonomy)
-//get price
 //get description
 //design changes checker
-
-
-function get_price($str){
-	global $price_marks;
-	foreach($price_marks as $mark){
-		if(stristr($str, $mark)) {
-			$pattern = '|(\d+)[^\d]*'.$mark.'|';
-			$r = preg_match($pattern, $str,$m);
-			if($r) return $m[1];
-		}
-	}
-	return false;
-}
-
-function clear_text($str){
-	$str = preg_replace('|(\d+)&nbsp;(\d+)|',"$1$2",$str);//avito prices
-	$str = preg_replace('|&.*;|U',' ',$str);
-	$str = preg_replace('|\x20+|',' ',$str);
-	$str = trim($str);
-	return $str;
-}
-
-function get_links($str){
-	$links = parse_array($str, '<a href="', '"', 1);
-	$uniq_links = array_unique($links);
-	return $uniq_links;
-}
-
-/*
-function get_imgs_dom($str){
-	$html = str_get_html($str);
-	foreach($html->find('img') as $element) {
-		$imgs[] = $element;// -> add something here 
-	}
-	$html->clear();
-	return $imgs;
-}*/
-
-function get_imgs($str){
-	$imgs = parse_array($str, '<img', '/>');
-	foreach($imgs as $img){
-		$img_links[] = get_attribute($img, 'src');
-	}
-	return $img_links;
-}
-
-function get_divs($str,$marks){
-	$html = str_get_html($str);
-	$i=0;
-	$sum=0;
-	foreach($html->find('div') as $element) {
-		$div = $element->innertext;
-		$m = sizeof($marks);
-		$c=0;
-		foreach($marks as $mark){
-			if(stristr($div, $mark)) $c++;
-		}
-		if ($c == $m) {
-			$divs[$i]['html'] = $div;
-			$divs[$i]['size'] = strlen($div);
-			$sum+= $divs[$i]['size'];
-			$i++;		
-		}
-	}
-	$html->clear();
-	
-	$average = round($sum/$i);
-	echo "[+]Sum $sum\n";
-	echo "[+]Average $average\n";
-	echo "[+]Found $i div blocks\n";
-
-	$g=0;
-	foreach($divs as $n=>$div){
-		if($div['size']<$average) {
-			$good_divs[] = $div['html'];
-			$g++;
-		}	
-	}
-	echo "[+]Found $g good div blocks\n";
-	return $good_divs;
-}
+//full block checker
 
 ?>
---
+++ libs/web_bots.php
@@ -0,0 +1,89 @@
+<?php
+require_once '..\libs\LIB_parse.php';
+require_once '..\libs\LIB_http.php';
+require_once '..\libs\simple_html_dom.php';
+
+$price_marks[] = 'руб.';
+$price_marks[] = 'рублей';
+
+function get_price($str){
+	global $price_marks;
+	foreach($price_marks as $mark){
+		if(stristr($str, $mark)) {
+			$pattern = '|(\d+)[^\d]*'.$mark.'|';
+			$r = preg_match($pattern, $str,$m);
+			if($r) return $m[1];
+		}
+	}
+	return false;
+}
+
+function clear_text($str){
+	$str = preg_replace('|(\d+)&nbsp;(\d+)|',"$1$2",$str);//avito prices
+	$str = preg_replace('|&.*;|U',' ',$str);
+	$str = preg_replace('|\x20+|',' ',$str);
+	$str = trim($str);
+	return $str;
+}
+
+function get_links($str){
+	$links = parse_array($str, '<a href="', '"', 1);
+	$uniq_links = array_unique($links);
+	return $uniq_links;
+}
+
+/*
+function get_imgs_dom($str){
+	$html = str_get_html($str);
+	foreach($html->find('img') as $element) {
+		$imgs[] = $element;// -> add something here 
+	}
+	$html->clear();
+	return $imgs;
+}*/
+
+function get_imgs($str){
+	$imgs = parse_array($str, '<img', '/>');
+	foreach($imgs as $img){
+		$img_links[] = get_attribute($img, 'src');
+	}
+	return $img_links;
+}
+
+function get_divs($str,$marks){
+	$html = str_get_html($str);
+	$i=0;
+	$sum=0;
+	foreach($html->find('div') as $element) {
+		$div = $element->innertext;
+		$m = sizeof($marks);
+		$c=0;
+		foreach($marks as $mark){
+			if(stristr($div, $mark)) $c++;
+		}
+		if ($c == $m) {
+			$divs[$i]['html'] = $div;
+			$divs[$i]['size'] = strlen($div);
+			$sum+= $divs[$i]['size'];
+			$i++;		
+		}
+	}
+	$html->clear();
+	
+	$average = round($sum/$i);
+	echo "[+]Sum $sum\n";
+	echo "[+]Average $average\n";
+	echo "[+]Found $i div blocks\n";
+
+	$g=0;
+	foreach($divs as $n=>$div){
+		if($div['size']<$average) {
+			$good_divs[] = $div['html'];
+			$g++;
+		}	
+	}
+	echo "[+]Found $g good div blocks\n";
+	return $good_divs;
+}
+
+?>
