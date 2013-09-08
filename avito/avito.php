6f89fc522b4fd1a5a4753a29308d9049afc3b874
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-08 10:20:19 +0400

    netbeans

--- avito/avito.php
+++ avito/avito.php
@@ -5,8 +5,8 @@ Avito.ru grabber and parser
 require_once '..\libs\web_bots.php';
 echo "\n[+] Started\n";
 
-$db_file = 'avito.gz';
-$debug_file = 'debug.html';
+$db_file = '..\db\avito.gz';//local, one run data base
+$debug_file = 'debug.html';//cache
 $url = "http://www.avito.ru/sankt-peterburg/ohota_i_rybalka?metro_id=170&user=1&s=1";
 $in = http_get_debug($url,$debug_file);
 if(!$in) {
@@ -14,39 +14,40 @@ if(!$in) {
 	exit;
 }	
 $tidy = tidy_html($in);
+//base url finder
 //add code page check and convert if needed
 
-
+//Marks for blocks
 $div_marks[] = 'img';
 $div_marks[] = 'руб';
-$good_divs = get_divs($tidy,$div_marks);
+$html_blocks = get_divs($tidy,$div_marks);//Get blocks from html
 $corrupt_blocks = 0;
 
-for($i=0;$i<count($good_divs);$i++){
+//Blocks to elements
+for($i=0;$i<count($html_blocks);$i++){
 	$fill = 0;
-	$data[$i]['imgs']= get_imgs($good_divs[$i]);
-	if(sizeof($data[$i]['imgs'])>0) $fill++;
-	$data[$i]['links']= get_links($good_divs[$i]);
-	if(sizeof($data[$i]['imgs'])>0) $fill++;
-	$data[$i]['raw_text']= strip_tags($good_divs[$i]);//Should I keep a raw text?
-	if(strlen($data[$i]['raw_text'])>0) $fill++;
-	$data[$i]['clear_text']= clear_text($data[$i]['raw_text']);
-	if(strlen($data[$i]['clear_text'])>0) $fill++;
-	$data[$i]['price']= get_price($data[$i]['clear_text']);
-	if(strlen($data[$i]['price'])>0) $fill++;
+	$blocks[$i]['imgs']= get_imgs($html_blocks[$i]);
+	if(sizeof($blocks[$i]['imgs'])>0) $fill++;
+	$blocks[$i]['links']= get_links($html_blocks[$i]);
+	if(sizeof($blocks[$i]['imgs'])>0) $fill++;
+	$blocks[$i]['raw_text']= strip_tags($html_blocks[$i]);//Should I keep a raw text?
+	if(strlen($blocks[$i]['raw_text'])>0) $fill++;
+	$blocks[$i]['clear_text']= clear_text($blocks[$i]['raw_text']);
+	if(strlen($blocks[$i]['clear_text'])>0) $fill++;
+	$blocks[$i]['price']= get_price($blocks[$i]['clear_text']);
+	if(strlen($blocks[$i]['price'])>0) $fill++;
 	if($fill<5) {
 		echo "[-] Corrupted block: $i\n";
 		$corrupt_blocks++;
 	} else {
-		$data[$i]['hash'] = md5($data[$i]['clear_text']);
+		$blocks[$i]['hash'] = md5($blocks[$i]['clear_text']);
 	}
 	//break;
 }
 
 echo "[i] Corrupted blocks: $corrupt_blocks\n";
-var_dump($data);
+//var_dump($blocks);
 	
-if(save_json($db_file,$data)) echo "[+] Saved\n";
-// Or add to mysql db
+if(save_json($db_file,$blocks)) echo "[+] Saved\n";
 
 ?>
--- avito/m2.php
+++ avito/m2.php
@@ -6,14 +6,46 @@ require_once '..\libs\web_bots.php';
 require_once '..\libs\taxonomy.php';
 echo "\n[+] Started\n";
 
-$db_in_file = 'avito.gz';
-$db_tagged_file = 'avito-tagged.gz';
-$db_stats_file = 'avito-stats.gz';
+$db_in_file = '..\db\avito.gz';
+$db_tagged_file = '..\db\avito-tagged.gz';//global data base
+$db_stats_file = '..\db\avito-stats.gz';
 if(file_exists($db_in_file)){
-	$blocks = load_json($db_in_file);
+	$new_blocks = load_json($db_in_file);
 }
-$size = count($blocks);
-echo "[+] Read $size elements\n";
+$new_size = count($new_blocks);
+echo "[+] Read $new_size new blocks\n";
+
+if(file_exists($db_tagged_file)){
+	$global_blocks = load_json($db_tagged_file);
+}
+$global_size = count($global_blocks);
+echo "[+] Read $global_size global blocks\n";
+
+//Filter Uniqs
+//Optimization needed
+/*
+Go throw new blocks
+Check hash of new block throw global blocks
+Equal - next new block
+Not equal - next global block
+Not found - uniq +1
+*/
+$uniq_blocks = 0;
+foreach($new_blocks as $new_block){
+	$i=0;
+	foreach($global_blocks as $global_block){
+		if($new_block['hash'] == $global_block['hash']) break;
+		$i++;
+	}
+	if($i==$global_size) {
+		$uniq_blocks++;
+		$global_block[] = $new_block;//Order of global blocks?
+	}	
+}
+echo "[+] Uniq Blocks found: $uniq_blocks\n";
+
+$blocks = $new_blocks;
+$size = $new_size;
 
 //Clustering
 $untagged_blocks = 0;
@@ -53,7 +85,7 @@ foreach($clusters as $category=>$tagged_blocks){
 	$stats[$category]['low_limit'] = round($stats[$category]['average']-3*$stats[$category]['standard_deviation']);
 	$stats[$category]['high_limit'] = round($stats[$category]['average']+3*$stats[$category]['standard_deviation']);
 }
-var_dump($stats);
+//var_dump($stats);
 
 if(save_json($db_tagged_file,$blocks)) echo "[+] Saved tagged blocks\n";
 if(save_json($db_stats_file,$stats)) echo "[+] Saved stats\n";
