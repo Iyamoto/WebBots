2c6c302227ce2aecc5c28a44d9b48a891c9af492
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-11 19:40:34 +0400

    todo change

--- avito/report.php
+++ avito/report.php
@@ -7,14 +7,14 @@ require_once '..' . DIRECTORY_SEPARATOR . 'libs' . DIRECTORY_SEPARATOR . 'web_bo
 echo "\n[+] Started\n";
 
 $db_dir = '..' . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR . 'db';
-$db_global_file = $db_dir . DIRECTORY_SEPARATOR . 'avito-global.gz'; //global data base
+//$db_global_file = $db_dir . DIRECTORY_SEPARATOR . 'avito-global.gz'; //global data base
 $db_stats_file = $db_dir . DIRECTORY_SEPARATOR . 'avito-stats.gz';
 
 //Read stats
 $stats = read_db_from_file($db_stats_file);
 if ($stats) {
     $stats_size = sizeof($stats);
-    echo "[+] Read $stats_size stats\n";
+    echo "[+] Read $stats_size stats blocks\n";
 }
 else
     exit('Problem with stats');
